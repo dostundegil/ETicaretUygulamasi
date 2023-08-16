8d6d3a927238877b74876f191c3c1221f7398af6
Ali Kaan YAYLA <kaan_sv@hotmail.com>
2023-08-16 17:59:07 +0300

    first commit

---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new(){Id = Guid.NewGuid(), Name = "Product1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10}
+            });
+
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+    }
+}
+
